f8edcbe52e778759cbd5068f786923774f9a07ff
Karunlk <223516111+Karunlk@users.noreply.github.com>
2026-09-22 21:41:24 +0530

    Completed Stage 5 Task 3

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 
 CC="gcc"
 
-STAGE="stage4"
-TASK="exe2"
+STAGE="stage5"
+TASK="task3"
 ADDRESS="$HOME/Documents/compiler-lab/$STAGE/$TASK/"
 LEXFILE="$TASK.l"
 YACCFILE="$TASK.y"
@@ -18,4 +18,11 @@ lex label_translate.l
 $CC lex.yy.c -o label_translate
 ./label_translate output.o
 
+XSMADDRESS="$HOME/Documents/compiler-lab/xsm_expl"
+RUNADDRESS="/$STAGE/$TASK"
+
+cd $XSMADDRESS
+./xsm -l library.lib -e ../$RUNADDRESS/output.xsm
+
+rm -rf ../$ADDRESS/output.xsm
 rm -rf lex.yy.c y.tab.c y.tab.h a.out output.o label_translate
